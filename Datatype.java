32594315059a94b2af82a98ec534abad4cc70c66
Tata Anand Kishore <anandkishoretata@gmail.com>
2026-07-26 20:01:30 +0530

    Initial commit

---
+++ Datatype.java
@@ -0,0 +1,24 @@
+class Datatype
+{
+    public static void main(String args[])
+    {
+        Boolean aa=true;
+        Boolean bb=false;
+        char a = 'G';
+        int i = 89;
+        byte b = 4;
+        short s = 56;
+        double d = 4.355453537;
+        float f = 4.7333434f;
+        long l = 12121;
+        System.out.println("char:" + a);
+        System.out.println("integer:" + i);
+        System.out.println("byte:" + b);
+        System.out.println("short:" + s);
+        System.out.println("float:" + f);
+        System.out.println("double:" + d);
+        System.out.println("long: " + 1);
+        System.out.println("Bool:" + aa);
+        System.out.println("Bool:" + bb);
+    }
+}
