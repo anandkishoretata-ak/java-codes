28081e8891fb81848f10d078e7c450ce6acd68de
Tata Anand Kishore <anandkishoretata@gmail.com>
2026-07-26 20:03:45 +0530

    Initial commit

---
+++ Variables.java
@@ -0,0 +1,12 @@
+public class Variables {
+    public static void main(String[] args) {
+
+        // Declaring a variable
+        // Syntax: dataType variableName = value;
+
+        int anand = 123;
+
+        // Accessing and printing the variable
+        System.out.println(anand);
+    }
+}
